0da3fd41878e104bbf9ad1c328c9a78c8f4d8e42
MaddalaNeeleshsai <144872624+MaddalaNeeleshsai@users.noreply.github.com>
2024-12-07 13:11:52 +0530

    Add files via upload

---
+++ spring-di-demo/src/main/java/com/klef/jfsd/exaam/ClitenDemo.java
@@ -0,0 +1,5 @@
+package com.klef.jfsd.exaam;
+
+public class ClitenDemo {
+
+}
---
+++ spring-di-demo/src/main/java/com/klef/jfsd/exaam/Instructor.java
@@ -0,0 +1,19 @@
+package com.klef.jfsd.exaam;
+
+public class Instructor {
+	private Integer instructorId;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    public Instructor(Integer instructorId, String name, String email, String phoneNumber) {
+        this.instructorId = instructorId;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+    @Override
+    public String toString() {
+        return "Instructor [instructorId=" + instructorId + ", name=" + name + ", email=" + email + 
+               ", phoneNumber=" + phoneNumber + "]";
+    }
+}
